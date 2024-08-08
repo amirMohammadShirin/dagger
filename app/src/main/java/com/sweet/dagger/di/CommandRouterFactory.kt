4fe9a507541fdb37f614fe039075fe36528b46b9
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-08-08 23:06:07 +0330

    add UserCommandModule to router component

--- app/src/main/java/com/sweet/dagger/di/CommandRouterFactory.kt
+++ app/src/main/java/com/sweet/dagger/di/CommandRouterFactory.kt
@@ -2,14 +2,18 @@ package com.sweet.dagger.di
 
 import com.sweet.dagger.model.router.CommandRouter
 import dagger.Component
+import javax.inject.Singleton
+
 
 @Component(
     modules = [
         LoginModule::class,
         OutPutterModule::class,
-        HelloWordModule::class
+        HelloWordModule::class,
+        UserCommandModule::class
     ]
 )
+@Singleton
 interface CommandRouterFactory {
 
     fun router(): CommandRouter
