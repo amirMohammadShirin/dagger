ffbe86ad54fa9306b49c5673586958498259df97
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-08-08 18:52:16 +0330

    add HelloWordModule to CommandRouter Component

--- app/src/main/java/com/sweet/dagger/di/CommandRouterFactory.kt
+++ app/src/main/java/com/sweet/dagger/di/CommandRouterFactory.kt
@@ -3,7 +3,9 @@ package com.sweet.dagger.di
 import com.sweet.dagger.model.CommandRouter
 import dagger.Component
 
-@Component
+@Component(
+    modules = [HelloWordModule::class]
+)
 interface CommandRouterFactory {
 
     fun router(): CommandRouter
