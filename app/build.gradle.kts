24e4a77c838a9e8bbccb340e9949feb4b9b7eedf
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-08-08 18:13:38 +0330

    add dagger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,7 +37,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.dagger)
+    kapt(libs.dagger)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
