fe7290e60e8a88dc24f30e69d33a31bd59b7097d
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-08-08 20:35:20 +0330

    put commands to map

--- app/src/main/java/com/sweet/dagger/di/HelloWordModule.kt
+++ app/src/main/java/com/sweet/dagger/di/HelloWordModule.kt
@@ -4,11 +4,15 @@ import com.sweet.dagger.model.Command
 import com.sweet.dagger.model.command.HelloWorldCommand
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import dagger.multibindings.StringKey
 
 @Module
 abstract class HelloWordModule {
 
     @Binds
+    @IntoMap
+    @StringKey("hello")
     abstract fun bindHelloWorldCommand(helloWorldCommand: HelloWorldCommand) : Command
 
 }
