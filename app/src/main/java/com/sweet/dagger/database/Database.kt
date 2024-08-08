085c5a0a9eaf671c80b68d1b8ec7a5cd99658821
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-08-08 23:06:47 +0330

    singleton database put a mock user to database

--- app/src/main/java/com/sweet/dagger/database/Database.kt
+++ app/src/main/java/com/sweet/dagger/database/Database.kt
@@ -1,14 +1,23 @@
 package com.sweet.dagger.database
 
 import com.sweet.dagger.model.Account
+import java.math.BigDecimal
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class Database @Inject constructor() {
 
     private val accounts: MutableMap<String, Account> = mutableMapOf()
 
+    init {
+        accounts["amir"] = Account("amir", BigDecimal(50))
+    }
+
     fun getAccount(id: String): Account? = accounts[id]
 
-
+    fun upsertAccount(account: Account) {
+        accounts[account.id] = account
+    }
 
 }
